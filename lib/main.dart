06ae2e944004be2dc968dae6b9e374fc89cd5acd
Ibrahim <i-m-6@hotmail.com>
2023-02-07 14:39:42 +0300

    format: formating code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: Center(child: Text("YA HALA ..."),),),));
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text("YA HALA ..."),
+      ),
+    ),
+  ));
 }
