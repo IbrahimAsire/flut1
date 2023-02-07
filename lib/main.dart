c68e3c90dfdc7250f31382a13727e39299f80010
Ibrahim <i-m-6@hotmail.com>
2023-02-07 15:03:27 +0300

    feat: add a new scaffold and widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      appBar: AppBar(title: Text("THIS'S MY FIRS App ..")),
       body: Center(
         child: Text("YA HALA ..."),
       ),
