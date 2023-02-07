41fb5c761796b650fe8c846c0e8ae321fbe12fc3
Ibrahim <i-m-6@hotmail.com>
2023-02-07 14:38:51 +0300

    feat: add feture to screen by use scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Center(child: Text("YA HALA ..."),),));
+  runApp(MaterialApp(home: Scaffold(body: Center(child: Text("YA HALA ..."),),),));
 }
-
