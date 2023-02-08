5aaaf0dd9d3048f0800ae146064dfd390c0d7fcf
Ibrahim <i-m-6@hotmail.com>
2023-02-08 12:08:29 +0300

    feat: add menu to App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Text("THIS'S MY FIRS App ..")),
+      appBar: AppBar(title: Text("THIS'S MY FIRS App .."),
+      leading: Icon(
+          Icons.menu,
+      ),),
       body: Center(
         child: Text("YA HALA ..."),
       ),
