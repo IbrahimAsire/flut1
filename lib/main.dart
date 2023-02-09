ace037f6a6c5f2846afde8dc9889b2c740cc7043
Ibrahim <i-m-6@hotmail.com>
2023-02-09 15:04:40 +0300

    feat: learn and add background feature

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.purple,
       appBar: AppBar(title: Text("THIS'S MY FIRS App .."),
       leading: Icon(
           Icons.menu,
