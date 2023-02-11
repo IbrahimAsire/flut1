9dc70d9b72b99665a19ccaa86b2c7a4fc35755f9
Ibrahim <i-m-6@hotmail.com>
2023-02-11 17:42:31 +0300

    feat: use hot reload feature

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Colors.purple,
-      appBar: AppBar(title: Text("THIS'S MY FIRS App .."),
-      leading: Icon(
+      appBar: AppBar(
+        title: Text("THIS'S MY FIRS App .."),
+        leading: Icon(
           Icons.menu,
-      ),),
+        ),
+      ),
       body: Center(
         child: Text("YA HALA ..."),
       ),
-    ),
-  ));
+    );
+  }
 }
