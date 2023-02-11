ebd1446265d7f9f644cf45e7ba337882e42e2bb4
Ibrahim <i-m-6@hotmail.com>
2023-02-11 17:45:10 +0300

    style: change background by hot reloab buttton

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple,
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         title: Text("THIS'S MY FIRS App .."),
         leading: Icon(
