0543055c7a44e27f1fae98fd6cf6858bab5c075f
Ibrahim <i-m-6@hotmail.com>
2023-02-17 19:13:47 +0300

    styling

--- lib/main.dart
+++ lib/main.dart
@@ -15,20 +15,24 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(),
-            Text(
-              "Start",
-              style: TextStyle(fontSize: 23, fontWeight: FontWeight.w500),
-            ),
-            Text(
-              "sgasghfg fgf ggdfg fgfghsgsghjg kjhg ljh;usfh sjfjgldsfj lfggs  gflg ggffg lgfgfg gfglf gfggl llghguhg, hjg hh hjhgghg fgfgsfgfgfgfgf hgggmhggyeegvbbc sdvefvjfgxbcv",
-              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w300, color: Colors.grey[800]),
-            ),
-            Row(),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(17.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(),
+              Text(
+                "Start",
+                style: TextStyle(fontSize: 23, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 8),
+              Text(
+                "sgasghfg fgf ggdfg fgfghsgsghjg kjhg ljh;usfh sjfjgldsfj lfggs  gflg ggffg lgfgfg gfglf gfggl llghguhg, hjg hh hjhgghg fgfgsfgfgfgfgf hgggmhggyeegvbbc sdvefvjfgxbcv",
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.w300, color: Colors.grey[800]),
+              ),
+              Row(),
+            ],
+          ),
         ),
       ),
     );
